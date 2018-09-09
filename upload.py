6505702779fc3c37ac2666662b96b480d6881d1e
LankyCyril <cyril@ro.ru>
2018-09-09 02:52:29 -0400

    PyPI metadata and upload boilerplate

---
+++ upload.py
@@ -0,0 +1,9 @@
+from imp import load_source
+from subprocess import call
+
+meta = load_source("_", "./setup.py").meta
+tarball = "dist/{}-{}.tar.gz".format(meta.__name__, meta.__version__)
+
+call(["pandoc", "--from=markdown", "--to=rst", "-o", "README.rst", "README.md"])
+call(["python3", "setup.py", "sdist"])
+call(["twine", "upload", "-r", "pypi", tarball])
